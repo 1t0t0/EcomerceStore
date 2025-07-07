1bc60a7c90304dd1bd409db4f26882d6ed929ba8
Ttoto Phandolack <totophandolack915@gmail.com>
2025-07-07 10:12:29 +0700

    add theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
+
 
 const inter = Inter({
   subsets: ["latin"],})
 
 export const metadata: Metadata = {
   title: {
-  template: `%s | Prostore`,
+  template: `%s | EcomerceStore`,
   default: APP_NAME,
   },
   description:APP_DESCRIPTION,
@@ -21,11 +23,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} antialiased`}
       >
+        <ThemeProvider 
+        attribute='class'
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
