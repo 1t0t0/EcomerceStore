91ff468fba03fb51863266d18c826c64b0765528
Ttoto Phandolack <totophandolack915@gmail.com>
2025-08-16 13:15:03 +0700

    fixed params single product type error

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -8,7 +8,13 @@ const ProductImages = ({images}:{images:string[]}) => {
     const [current,setCurrent] = useState(0)
 
     return ( <div className="space-y-4">
-        <Image src={images[current]} alt="product images" width={1000} height={1000} className="min-h-[300px]" object-cover object-center/>
+        <Image 
+            src={images[current]} 
+            alt="product images" 
+            width={1000} 
+            height={1000} 
+            className="min-h-[300px] object-cover object-center"
+        />
         <div className="flex">
             {images.map((image,index)=>(
                 <div key={image} onClick={()=> setCurrent(index)} className={cn('border mr-2 cursor-pointer hover:border-orange-600', current === index && 'border-orange-500')}>
