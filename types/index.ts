dab3f5fe85913bb5bdce64209747f5f468d23ca8
Ttoto Phandolack <totophandolack915@gmail.com>
2025-08-16 13:34:33 +0700

    fixed type error in root page

--- types/index.ts
+++ types/index.ts
@@ -2,7 +2,9 @@ import z from "zod";
 import { insertProductSchema } from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductSchema> & {
-  id:string;
-  ratting:string;
-  createdAt:Date
+  id: string;
+  rating: string;  // แก้จาก ratting เป็น rating
+  numReviews: number;
+  description: string;
+  createAt: Date;  // ใช้ createAt ตามที่มีใน database schema
 }
