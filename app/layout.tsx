4a231a115e779cbdb8bb51ebdbd715aff92ba597
Ttoto Phandolack <totophandolack915@gmail.com>
2025-09-13 13:31:49 +0700

    Add to cart components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
 
 
 const inter = Inter({
@@ -33,6 +34,7 @@ export default function RootLayout({
         enableSystem
         disableTransitionOnChange>
         {children}
+        <Toaster/>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { CartItem } from "@/types";
+import { toast } from "sonner";
+import { addItemToCart } from "@/lib/actions/cart.action";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter()
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item)
+
+    if (!res.success) {
+   
+      toast.error(res.message)
+      return
+    }
+
+    toast.success(`${item.name} added to cart`, {
+      action: {
+        label: "Go To Cart",
+        onClick: () => router.push("/cart"),
+      },
+    })
+  }
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      Add to Cart
+    </Button>
+  )
+}
+
+export default AddToCart;
