21e5ac83027b564d197eb5ebc1805d827c6a81aa
Ttoto Phandolack <totophandolack915@gmail.com>
2025-10-30 07:52:16 +0700

    Admin Overview Display Data

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -300,7 +300,7 @@ export async function getOrderSummary() {
     });
 
     // Get monthly sales
-    const salesDataRaw = await prisma.$queryRaw<Array<{month:string; totalSales: Prisma.Decimal}>>`SELECT to_char("createAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales" FROM "Order" GROUP BY to_char("createAt", 'MM/YY')`;
+    const salesDataRaw = await prisma.$queryRaw<Array<{month:string; totalSales: Prisma.Decimal}>>`SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales" FROM "Order" GROUP BY to_char("createdAt", 'MM/YY')`;
 
     const salesData: SaleDataType = salesDataRaw.map((entry) => ({
         month: entry.month,
